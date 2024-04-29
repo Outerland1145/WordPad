71e8825509e413ff793f0ffcb6fe97a9bf4f99dc
Outerland1145 <162087937+Outerland1145@users.noreply.github.com>
2024-04-29 13:33:12 +0800

    完成版面配置

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,58 @@ namespace WordPad
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Load = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Load
+            // 
+            this.Load.Font = new System.Drawing.Font("新細明體", 18F);
+            this.Load.Location = new System.Drawing.Point(12, 12);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(106, 36);
+            this.Load.TabIndex = 0;
+            this.Load.Text = "讀取";
+            this.Load.UseVisualStyleBackColor = true;
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("新細明體", 18F);
+            this.Save.Location = new System.Drawing.Point(124, 12);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(106, 36);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "儲存";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 384);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Load);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
